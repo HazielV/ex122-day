b9bbe7c173de3d373295f8aa38280214f8fae727
HazielV <hazielvictor5@gmail.com>
2024-12-16 02:01:53 -0400

    arreglo

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,21 @@ import {
 function App() {
   const items = [
     {
-      src: '/bebidas',
-      altText: 'Slide 1',
-      caption: 'Slide 1',
+      src: '/carnes/carne4.webp',
+      altText: 'Producto 1',
+      caption: 'Producto 1',
       key: 1,
     },
     {
-      src: 'https://picsum.photos/id/456/1200/400',
-      altText: 'Slide 2',
-      caption: 'Slide 2',
+      src: '/frutas/fruta5.jpg',
+      altText: 'Producto 2',
+      caption: 'Producto 2',
       key: 2,
     },
     {
-      src: 'https://picsum.photos/id/678/1200/400',
-      altText: 'Slide 3',
-      caption: 'Slide 3',
+      src: '/panaderia/pan3.png',
+      altText: 'Producto 3',
+      caption: 'Producto 3',
       key: 3,
     },
   ]
@@ -52,12 +52,33 @@ function App() {
   const slides = items.map((item) => {
     return (
       <CarouselItem
+        className="overflow-hidden text-black"
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
         key={item.src}
+        style={{
+          maxWidth: '60rem', // Ajusta el ancho máximo
+          margin: '0 auto',
+          height: '400px', // Define una altura fija para el carrusel
+          overflow: 'hidden',
+        }}
       >
-        <img src={item.src} alt={item.altText} />
+        <img
+          src={item.src}
+          alt={item.altText}
+          style={{
+            width: '100%', // Ajusta al ancho del contenedor
+            height: 'auto',
+            maxHeight: '35rem', // Mantén la proporción
+            objectFit: 'cover', // O ajusta según tus necesidades
+            objectPosition: 'start',
+          }}
+        />
         <CarouselCaption
+          style={{
+            color: 'black', // Cambia el color del texto a negro
+          }}
+          className="text-black"
           captionText={item.caption}
           captionHeader={item.caption}
         />
@@ -67,28 +88,41 @@ function App() {
   return (
     <div>
       <MyNav />
-      <Container className="mt-4">
+      <Container className="mt-4 text-dark">
         <Row>
           <Col>
             <h2 className="text-center mb-4">Bienvenido a la tienda Patito</h2>
-            <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-              <CarouselIndicators
-                items={items}
+            <div style={{ background: '#E8E8E9' }}>
+              <Carousel
+                style={{ maxWidth: '60rem', margin: '0 auto', color: 'F8F9FA' }}
                 activeIndex={activeIndex}
-                onClickHandler={goToIndex}
-              />
-              {slides}
-              <CarouselControl
-                direction="prev"
-                directionText="Previous"
-                onClickHandler={previous}
-              />
-              <CarouselControl
-                direction="next"
-                directionText="Next"
-                onClickHandler={next}
-              />
-            </Carousel>
+                next={next}
+                previous={previous}
+              >
+                <CarouselIndicators
+                  className="bg-secondary-subtle"
+                  items={items}
+                  activeIndex={activeIndex}
+                  onClickHandler={goToIndex}
+                  style={{ color: 'black' }}
+                />
+                {slides}
+                <CarouselControl
+                  className="bg-secondary-subtle"
+                  direction="prev"
+                  directionText="Previous"
+                  onClickHandler={previous}
+                  style={{ color: 'black' }}
+                />
+                <CarouselControl
+                  className="bg-secondary-subtle"
+                  direction="next"
+                  directionText="Next"
+                  onClickHandler={next}
+                  style={{ color: 'black', backgroundColor: '#040302' }}
+                />
+              </Carousel>
+            </div>
           </Col>
         </Row>
       </Container>
